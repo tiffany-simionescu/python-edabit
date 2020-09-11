5420cf587d34042ec847fdb3cc7c3ceb27da029d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-09-11 09:37:21 -0400

    fizz_buzz

--- medium.py
+++ medium.py
@@ -4,3 +4,14 @@ def int_to_str(n):
 
 def str_to_int(s):
 	return str(s)
+
+# FizzBuzz
+def fizz_buzz(num):
+	if num % 3 == 0 and num % 5 == 0:
+		return "FizzBuzz"
+	elif num % 5 == 0:
+		return "Buzz"
+	elif num % 3 == 0:
+		return "Fizz"
+	else:
+		return str(num)
